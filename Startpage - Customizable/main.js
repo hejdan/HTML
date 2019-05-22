9309f7e5b17a4043c31dee00d7f3a07c1c9a432d
Hejdan <herrm0rris@hotmail.com>
2019-05-22 16:17:53 +0200

    Updated Customizable Startpage

--- Startpage - Customizable/main.js
+++ Startpage - Customizable/main.js
@@ -1,23 +1,68 @@
 //All category names
 var Names = document.querySelectorAll("[id='Category-Name']");
 
-//All catergory links
+//All category links
 var Links = document.querySelectorAll("[id='Category-Links']");
 
 
 //Adds hyperlinks to category
 function AddItem() {
+	//Link array
+	var AllLinks = [];
+	
     var Item = document.createElement("a")
-    var ItemName = document.createTextNode("Link");
+    var ItemName = document.createTextNode("New Link");
     Item.setAttribute('href', "#");
     Item.appendChild(ItemName);
     document.getElementById("Category-Links").appendChild(Item);
+	AllLinks.push(Item);
+	console.log(AllLinks);
 }
 
-Names.forEach(array =>{
-    addEventListener("click", ChangeName);
-})
+    //addEventListener("click", ChangeName);
 
 function ChangeName() {
-   document.getElementById("Category-Name").innerHTML ="Hej";
+   document.getElementById("Category-Name").innerHTML ="New Category";
+}
+
+function AddCategory(){
+	//Category array
+	var AllCategories = [];
+	
+	//Creates new category
+	var Category = document.createElement("div");
+	Category.setAttribute("id", "Category");
+    document.getElementById("Categories").appendChild(Category);
+
+	
+	//Creates the title-div in category
+	var CategoryTitle = document.createElement("div");
+	CategoryTitle.setAttribute("id", "Category-Name");
+	document.getElementById("Category").appendChild(CategoryTitle);
+    Category.appendChild(CategoryTitle);
+	
+	//Creates the title in category
+	var CategoryTitleNameItem = document.createElement("h3");
+	CategoryTitleNameItem.setAttribute("id", "Category-Name");
+	var	CategoryTitleName = document.createTextNode("No Name");
+	CategoryTitleNameItem.appendChild(CategoryTitleName);
+	Category.appendChild(CategoryTitleNameItem);
+    
+    //Creates the link-div in category
+    var CategoryLinks = document.createElement("div");
+    CategoryLinks.setAttribute("id", "Category-Links");
+    document.getElementById("Category").appendChild(CategoryLinks);
+    
+    //Creates the add link button
+    var AddLinkButton = document.createElement("button");
+    AddLinkButton.setAttribute("onclick", "AddItem()");
+    AddLinkButton.setAttribute("id", "LinkButton");
+    AddLinkButtonName = document.createTextNode("+");
+    AddLinkButton.appendChild(AddLinkButtonName);
+    document.getElementById("Category-Links").appendChild(AddLinkButton);
+    Category.appendChild(AddLinkButton);
+
+	
+	AllCategories.push(Category);
+    console.log(AllCategories.length);
 }
